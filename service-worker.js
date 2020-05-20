c2bdecaeefa248f20797f04b637647b7456c5cb0
Nofrets Poai <opetstudio@gmail.com>
2020-05-20 19:10:05 +0700

    bugfixing lgi

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ade00e692c3b7d2c6d93d81a95ff116.js"
+  "/precache-manifest.67d6de7db6b52b9024e0c4cb83bfdc7f.js"
 );
 
 self.addEventListener('message', (event) => {
