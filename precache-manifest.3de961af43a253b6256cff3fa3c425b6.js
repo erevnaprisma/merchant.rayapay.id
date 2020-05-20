a8dda754f848220cd5098a9a42d0cdcab9613847
Nofrets Poai <opetstudio@gmail.com>
2020-05-20 18:32:48 +0700

    init

---
+++ precache-manifest.3de961af43a253b6256cff3fa3c425b6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d539a58584b7b52a27310a8275f3f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fbb8e784542ab4aa5b7",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "64b3ebb221635f82ea43",
+    "url": "/static/css/main.5044626b.chunk.css"
+  },
+  {
+    "revision": "6fbb8e784542ab4aa5b7",
+    "url": "/static/js/2.40e71ca4.chunk.js"
+  },
+  {
+    "revision": "eff7558e2610b9a36fe40d487b00d7e7",
+    "url": "/static/js/2.40e71ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b3ebb221635f82ea43",
+    "url": "/static/js/main.66808438.chunk.js"
+  },
+  {
+    "revision": "b357872a37ef9607793f",
+    "url": "/static/js/runtime-main.fdc481f0.js"
+  },
+  {
+    "revision": "28f7e31d41f353b3aaff1236c7108479",
+    "url": "/static/media/AdminLTELogo.28f7e31d.png"
+  },
+  {
+    "revision": "dc6d7a467091aba3f50c1f7dd192f880",
+    "url": "/static/media/defaultBannerMerchant.dc6d7a46.jpg"
+  },
+  {
+    "revision": "52777fdb08e418fe21ce417b233538dd",
+    "url": "/static/media/jcb-icon.52777fdb.png"
+  },
+  {
+    "revision": "af03b492afb0d85bfd531e2fe6a525e5",
+    "url": "/static/media/logorayapay.af03b492.png"
+  },
+  {
+    "revision": "118604c90d0e1fc30d96dee2203c13ea",
+    "url": "/static/media/mastercard-icon.118604c9.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/static/media/octocat.0ba2aa20.png"
+  },
+  {
+    "revision": "780f5b05b5c6acdf5a825b742d628f6f",
+    "url": "/static/media/pcidss.780f5b05.png"
+  },
+  {
+    "revision": "dd033ce27d4ecd99ce16780caa8ff56b",
+    "url": "/static/media/plinkpayment.dd033ce2.png"
+  },
+  {
+    "revision": "dbf30252419fb0ada0e900e875cc29ca",
+    "url": "/static/media/qris.dbf30252.jpg"
+  },
+  {
+    "revision": "d576b03b9a95c59b15a2d906cd48bc10",
+    "url": "/static/media/qrmerchantinstitute.d576b03b.jpg"
+  },
+  {
+    "revision": "bcbd98622dc6eb4de09ae2aa7a64818f",
+    "url": "/static/media/sslsecurity.bcbd9862.png"
+  },
+  {
+    "revision": "209fab20fdb1000343fab6aa756de191",
+    "url": "/static/media/user.209fab20.png"
+  }
+]);
